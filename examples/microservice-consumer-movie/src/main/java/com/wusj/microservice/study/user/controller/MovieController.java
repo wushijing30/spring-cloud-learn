48ca94bb9eb684fdadac60de38ef3ef5c10c19ec
wushijing30 <wushijing30@163.com>
2018-03-31 16:56:06 +0800

    消费者

---
+++ examples/microservice-consumer-movie/src/main/java/com/wusj/microservice/study/user/controller/MovieController.java
@@ -0,0 +1,22 @@
+package com.wusj.microservice.study.user.controller;
+
+import com.wusj.microservice.study.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by wusj on 2018/3/31.
+ */
+@RestController
+public class MovieController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("user/{id}")
+    public User findById(@PathVariable Long id){
+        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+    }
+}
