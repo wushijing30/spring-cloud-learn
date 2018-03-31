3d97ae4cf2043025b701634e23b13f6b36516a17
wushijing30 <wushijing30@163.com>
2018-03-31 17:20:03 +0800

    将消费者注册到注册中心

--- examples/microservice-consumer-movie/src/main/java/com/wusj/microservice/study/user/controller/MovieController.java
+++ examples/microservice-consumer-movie/src/main/java/com/wusj/microservice/study/user/controller/MovieController.java
@@ -17,6 +17,6 @@ public class MovieController {
 
     @GetMapping("user/{id}")
     public User findById(@PathVariable Long id){
-        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+        return this.restTemplate.getForObject("http://localhost:8000/user/" + id, User.class);
     }
 }
